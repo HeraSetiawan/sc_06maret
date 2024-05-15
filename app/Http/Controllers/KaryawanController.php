7bb225efaa2bf0b13e605b29b0762820b37efb9b
Hera Setiawam <herasetiawan.hs@gmail.com>
2024-05-15 16:50:31 +0700

    skill test

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Karyawan;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class KaryawanController extends Controller
 {
@@ -114,9 +118,37 @@ class KaryawanController extends Controller
         return redirect('/karyawan')->with('notif', 'Berhasil Hapus Data');
     }
 
-    public function absen()
+    public function absen(Request $req)
     {
+        // simpan foto
+        $foto64 = Str::after($req->foto, ',');
+        $foto = base64_decode( $foto64);
+        $namaFoto = uniqid().'.jpg';
+        Storage::put('Avatars/'.$namaFoto, $foto);
+
+        if ($req->keterangan == "masuk") {
+            // absen masuk
+            DB::table('absen')->insert(
+                [
+                    'user_id' => Auth::user()->id,
+                    'foto_masuk' => $namaFoto,
+                    'lokasi_masuk' => $req->lokasi 
+                ]
+            );
+        } else {
+            // absen keluar
+            DB::table('absen')->where('user_id', Auth::user()->id)
+            ->whereDate('created_at', Carbon::today())
+            ->update(
+                [
+                    'foto_masuk' => $namaFoto,
+                    'lokasi_masuk' => $req->lokasi 
+                ]
+            );
+        }
         
+        return redirect()->back()->with('pesan', "Berhasil Absen $req->keterangan");
+        // lanjutkan dan perbaiki kode hingga berhasil absen masuk dan absen keluar
     }
 }
 
